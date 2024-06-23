3eb27f37623b749d9b38fec117e3ef31ffecd9a3
cedricmaciel <164974899+cedricmaciel@users.noreply.github.com>
2024-06-23 16:35:55 -0300

    Alteraçao de header e camera

--- wastier/App.js
+++ wastier/App.js
@@ -1,47 +1,93 @@
-import { View, Text, StyleSheet } from 'react-native'
+import React, { useRef } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
+export default function App() {
+  const cameraRef = useRef(null);
 
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const options = { quality: 0.5, base64: true };
+      const data = await cameraRef.current.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
 
-
-export default function App(){
   return (
-
-    <View style={styles.header}>
-      <View style={styles.menu}>
-      <Text style={styles.texto}>WasTier</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.menu}>
+          <Text style={styles.texto}>WasTier</Text>
+        </View>
       </View>
-      
+
+      <View style={styles.cameraContainer}>
+        <RNCamera
+          ref={cameraRef}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.auto}
+          androidCameraPermissionOptions={{
+            title: 'Permissão para usar a câmera',
+            message: 'Precisamos da sua permissão para usar a câmera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancelar',
+          }}
+        />
+        <TouchableOpacity onPress={takePicture} style={styles.capture}>
+          <Text style={{ fontSize: 14, color: "blue" }}> SCAN </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  header:{
-    flex:1,
-    backgroundColor: "green",
-    fonteSize: 100,
-    marginTop: 34
+  container: {
+    flex: 1,
+    backgroundColor: '#006400',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
   },
-
-  menu:{
-
-   
-    height: 80,
-
+  header: {
     width: '100%',
-    backgroundColor: "black",
-    justifyContent: 'center', // centraliza verticalmente
-    alignItems: 'center', // centraliza horizontalmente
-    
+    height: 110,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  menu: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   texto: {
-    color: "white",
-    fonteSize: 24,
-    
-  }
-
-})
-
-
-
-
+    color: 'white',
+    fontSize: 50,
+    marginTop: 10,
+  },
+  cameraContainer: {
+    width: '90%',
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    borderColor: 'white',
+    borderWidth: 2,
+    overflow: 'hidden', 
+  },
+  preview: {
+    flex: 1,
+    width: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});

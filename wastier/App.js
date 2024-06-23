ae1d97b2aecfd32a46818357a24e79dbb0a3e1c2
cedricmaciel <164974899+cedricmaciel@users.noreply.github.com>
2024-06-23 15:35:25 -0300

    header criado
    titulo e cabeçalho , background

--- wastier/App.js
+++ wastier/App.js
@@ -1,14 +1,47 @@
-import { View, Text, StyleSheet } from 'react-native';
-import { Camera } from 'expo-camera';
+import { View, Text, StyleSheet } from 'react-native'
+
+
 
 
 export default function App(){
   return (
-    <View style={{marginTop: 34}}>
-      <Text>Hello programa</Text>
-      <Text>Hello programa</Text>
-      <Text>Hello programa</Text>
+
+    <View style={styles.header}>
+      <View style={styles.menu}>
+      <Text style={styles.texto}>WasTier</Text>
+      </View>
+      
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  header:{
+    flex:1,
+    backgroundColor: "green",
+    fonteSize: 100,
+    marginTop: 34
+  },
+
+  menu:{
+
+   
+    height: 80,
+
+    width: '100%',
+    backgroundColor: "black",
+    justifyContent: 'center', // centraliza verticalmente
+    alignItems: 'center', // centraliza horizontalmente
+    
+  },
+  texto: {
+    color: "white",
+    fonteSize: 24,
+    
+  }
+
+})
+
+
+
 

0d3fda9015bc18946be5be5f9019a24bc5661436
cedricmaciel <164974899+cedricmaciel@users.noreply.github.com>
2024-06-23 14:07:54 -0300

    teste

---
+++ wastier/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Welcome to Home Screen!</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;

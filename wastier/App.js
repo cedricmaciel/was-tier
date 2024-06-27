913118d673fc1eda3bbe641b0e53e18a38c4e85d
cedricmaciel <164974899+cedricmaciel@users.noreply.github.com>
2024-06-27 14:29:09 -0300

    Update App.js

--- wastier/App.js
+++ wastier/App.js
@@ -129,10 +129,7 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     borderWidth: 5,
     borderColor: '#FFFFFF',
-    shadowColor: 'blue',  
-  shadowOffset: { width: 2, height: 2},  
-  shadowOpacity: 1,  
-  shadowRadius: 10,  
+      
   },
 
   text: {
